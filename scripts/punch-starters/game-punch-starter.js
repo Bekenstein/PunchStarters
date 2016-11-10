449a61438b1d71a2a610d7224e746617de36ca51
Georgi Georgiev <gogo4ds@gmail.com>
2016-11-10 15:12:38 +0200

    implemented GamePunchStarter class

--- scripts/punch-starters/game-punch-starter.js
+++ scripts/punch-starters/game-punch-starter.js
@@ -1,5 +1,16 @@
-class GamePunchStarter {
-	//TODO: Implement GamePunchStarter
+let BasePunchStarter = require('./base-punch-starter');
+
+class GamePunchStarter extends BasePunchStarter{
+	constructor(id, name, manufacturer, description,genres,targetPrice, technologiesUsed){
+	    super(id, name, manufacturer, description,genres,targetPrice);
+        this._technologiesUsed = technologiesUsed;
+    }
+    get technologiesUsed(){
+        if(!Array.isArray(this._technologiesUsed)){
+            throw new Error('technology used should be an Array');
+        }
+        return this._technologiesUsed;
+    }
 }
 
 module.exports = GamePunchStarter;
