6660028374b622a49b3b989a2a6ad2ac9c73a221
dantcho <dantcho@abv.bg>
2016-11-10 15:50:25 +0200

    base-punch Class

--- scripts/punch-starters/base-punch-starter.js
+++ scripts/punch-starters/base-punch-starter.js
@@ -1,5 +1,47 @@
 class BasePunchStarter {
+    checkStringValidaty(obj) {
+        if (typeof obj === 'string') return obj;
+        else throw new TypeError();
+    }
+
+    checkNumberValidaty(obj) {
+        if (typeof obj === 'number') return obj;
+        else throw new TypeError();
+    }
+
+    constructor(id, name, manufacturer, description, genres, targetPrice) {
+        if (new.target === BasePunchStarter) {
+            throw new TypeError("Cannot construct BasePunchStarter instances directly");
+        }
+        let _this = this;
+        this.id = (function (id) {
+            return _this.checkNumberValidaty(id);
+        })(id);
+
+        this.name = (function (name) {
+            return _this.checkStringValidaty(name);
+        })(name);
+
+        this.manufacturer = (function (manufacturer) {
+            return _this.checkStringValidaty(manufacturer);
+        })(manufacturer);
+        this.description = (function (description) {
+            return _this.checkStringValidaty(description);
+        })(description);
+        this.genres = (function (genres) {
+            let returnVal=[];
+            for(let index in genres){
+                 returnVal.push(_this.checkStringValidaty(genres[Number(index)]));
+            }
+            return returnVal;
+        })(genres);
+        this.targetPrice = (function (targetPrice) {
+            return _this.checkNumberValidaty(targetPrice);
+        })(targetPrice);
+
+    }
+
+
     //TODO: Implement BasePunchStarter
 }
-
 module.exports = BasePunchStarter;
