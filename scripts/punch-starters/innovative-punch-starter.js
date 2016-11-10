f7466352d29633bac8c6f1414b255394a0214a38
fiher <fihercho@gmail.com>
2016-11-10 15:23:35 +0200

    2clasesDown

--- scripts/punch-starters/innovative-punch-starter.js
+++ scripts/punch-starters/innovative-punch-starter.js
@@ -1,5 +1,8 @@
-class InnovativePunchStarter {
+let basePunchStarter = require("./base-punch-starter");
+class InnovativePunchStarter extends basePunchStarter {
 	//TODO: Implement InnovativePunchStarter
+    constructor(id,name,manufacturer,description,genres,targetPrice){
+        super(id,name,manufacturer,description,genres,targetPrice);
 }
 
 module.exports = InnovativePunchStarter;
--- scripts/punch-starters/movie-punch-starter.js
+++ scripts/punch-starters/movie-punch-starter.js
@@ -1,6 +1,23 @@
-
-class MoviePunchStarter {
+let basePunchStarter = require("./base-punch-starter");
+class MoviePunchStarter extends basePunchStarter {
 	//TODO: Implement MoviePunchStarter
+    constructor(id,name,manufacturer,description,genres,targetPrice,director,actor){
+        super(id,name,manufacturer,description,genres,targetPrice);
+        this._director = director;
+        this._actor = actor;
+    }
+    get director(){
+        if(typeof (this._director) != "string"){
+            throw new TypeError("opa");
+        }
+           return this._director;
+    }
+    get actor(){
+        if(!Array.isArray(this._actor)){
+            throw new TypeError("opa");
+        }
+        return this._actor;
+    }
 }
 
 module.exports = MoviePunchStarter;
