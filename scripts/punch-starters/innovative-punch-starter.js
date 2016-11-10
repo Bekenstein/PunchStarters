b00425a04c73cc381cdf3798cdf83ac17c722fda
fiher <fihercho@gmail.com>
2016-11-10 15:25:35 +0200

    BugFixing
    Opa

--- scripts/punch-starters/innovative-punch-starter.js
+++ scripts/punch-starters/innovative-punch-starter.js
@@ -1,8 +1,9 @@
 let basePunchStarter = require("./base-punch-starter");
 class InnovativePunchStarter extends basePunchStarter {
 	//TODO: Implement InnovativePunchStarter
-    constructor(id,name,manufacturer,description,genres,targetPrice){
-        super(id,name,manufacturer,description,genres,targetPrice);
+    constructor(id,name,manufacturer,description,genres,targetPrice) {
+        super(id, name, manufacturer, description, genres, targetPrice);
+    }
 }
 
 module.exports = InnovativePunchStarter;
