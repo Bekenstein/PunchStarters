1cdc08f36daa94305801db7724594cf9f23e1145
Georgi Georgiev <gogo4ds@gmail.com>
2016-11-11 17:24:16 +0200

    movie and game

--- scripts/core-models/functions/game.js
+++ scripts/core-models/functions/game.js
@@ -1,8 +1,26 @@
 function render(){
-
+    let html = '<label>Technologies:</label>' +
+        '<div class="list-holder"><select class="input-technologies"></select></div>' +
+        '<div><input class="new-technology" placeholder="Add technology..."></div>' +
+        '<div class="button-holder">' +
+            '<button class="add-technology-button">Add</button>' +
+            '<button class="remove-technology-button">Remove</button>' +
+        '</div>';
+    $('.individual-parameters').append(html)
 }
 
 function attachEvents(){
+    $('.add-actor-button').on('click', function () {
+        let input = $('.new-technology');
+        let tech = select.val();
+        $('.input-actors').append($('<option>').val(tech).text(tech));
+        input.val('');
+    });
 
+    $('.remove-actor-button').on('click', function () {
+        let select =$('.input-technologies');
+        let tech = select.find(':selected');
+        select.find(tech).remove();
+    });
 }
 module.exports = {render, attachEvents};
