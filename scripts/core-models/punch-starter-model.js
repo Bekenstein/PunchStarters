d238a45ea61c34e0b1a5217b1c6164a64d184697
dantcho <danchoivanovv@gmail.com>
2016-11-11 23:50:36 +0200

    k

--- scripts/core-models/punch-starter-model.js
+++ scripts/core-models/punch-starter-model.js
@@ -1,26 +1,26 @@
 class PunchStarterModel {
-	//TODO: Implement Punch Starter Model
-    constructor(){
+    //TODO: Implement Punch Starter Model
+    constructor() {
 
     }
 
-    render(punchStarter){
-     let html = `<div class="punch-starter-wrapper">                                          `+
-                  `<div class="punch-starter-caption">                                        `+
-                    `<label>${punchStarter.name}</label>                                        `+
-                    `<label>${punchStarter.constructor.name.replace('PunchStarter','')}</label> `+
-                  `</div>                                                                       `+
-                  `<div class="punch-starter-resume">                                         `+
-                    `<label>Manufacturer</label>                                                `+
-                    `<p>${punchStarter.manufacturer}</p>                                        `+
-                    `<label>Description</label>                                                 `+
-                    `<p>${punchStarter.description}</p>                                         `+
-                  `</div>                                                                       `+
-                  `<div class="punch-starter-lists">                                          `+
-                  `</div>                                                                       `+
-                  `<div class="punch-starter-progress">                                       `+
-                  `</div>                                                                       `+
-                `</div>                                                                         `;
+    render(punchStarter) {
+        let html = `<div class="punch-starter-wrapper">                                          ` +
+            `<div class="punch-starter-caption">                                        ` +
+            `<label>${punchStarter.name}</label>                                        ` +
+            `<label>${punchStarter.constructor.name.replace('PunchStarter', '')}</label> ` +
+            `</div>                                                                       ` +
+            `<div class="punch-starter-resume">                                         ` +
+            `<label>Manufacturer</label>                                                ` +
+            `<p>${punchStarter.manufacturer}</p>                                        ` +
+            `<label>Description</label>                                                 ` +
+            `<p>${punchStarter.description}</p>                                         ` +
+            `</div>                                                                       ` +
+            `<div class="punch-starter-lists">                                          ` +
+            `</div>                                                                       ` +
+            `<div class="punch-starter-progress">                                       ` +
+            `</div>                                                                       ` +
+            `</div>                                                                         `;
         console.log(html);
 
         $('.wrapper main').html(html);
@@ -28,37 +28,39 @@ class PunchStarterModel {
         this.renderProgress(punchStarter);
 
     }
-    attachEvents(punchStarter){
+
+    attachEvents(punchStarter) {
 
     }
-    renderLists(punchStarter){
-    let html = `<div>                                                                           `+
-                 `<label>Genres</label>                                                         `+
-                 `<ul>                                                                          `;
-        for(let genres of punchStarter.genres){
-            html+= `<li>${genres}</li>\n`;
+
+    renderLists(punchStarter) {
+        let html = `<div>                                                                           ` +
+            `<label>Genres</label>                                                         ` +
+            `<ul>                                                                          `;
+        for (let genres of punchStarter.genres) {
+            html += `<li>${genres}</li>\n`;
         }
-        html+= `</ul>                                                                           `+
-                `</div>                                                                         `;
-        switch(punchStarter.constructor.name.replace('PunchStarter','')){
+        html += `</ul>                                                                           ` +
+            `</div>                                                                         `;
+        switch (punchStarter.constructor.name.replace('PunchStarter', '')) {
             case 'Movie':
-                html += `<div>`+
-                          `<label>Actors</label>`+
-                          `<ul>`;
-                for(let actor of punchStarter.actor){
-                    html+= `<li>${actor}</li>\n`;
+                html += `<div>` +
+                    `<label>Actors</label>` +
+                    `<ul>`;
+                for (let actor of punchStarter.actor) {
+                    html += `<li>${actor}</li>\n`;
                 }
-                html+= `</ul>`+
-                        `</div>`;
-                html+=`<div>`+
-                        `<label>Director</label>`+
-                        `<p>${punchStarter.director}</p>`+
-                      `</div>`;
+                html += `</ul>` +
+                    `</div>`;
+                html += `<div>` +
+                    `<label>Director</label>` +
+                    `<p>${punchStarter.director}</p>` +
+                    `</div>`;
                 break;
             case 'Game':
-                html+= `<div>`+
-                        `<label>Technologies Used</label>`;
-                for(let technologies of punchStarter.technologiesUsed){
+                html += `<div>` +
+                    `<label>Technologies Used</label>`;
+                for (let technologies of punchStarter.technologiesUsed) {
                     html += `<li>${obj.technologies}</li>`;
                 }
                 html += `</div>`;
@@ -66,25 +68,25 @@ class PunchStarterModel {
             case 'Innovative':
                 break;
             case 'Food':
-                html += `<div>`+
-                        `<label>Ingredients</label>`;
-                for(let ingredients of punchStarter.ingredients){
+                html += `<div>` +
+                    `<label>Ingredients</label>`;
+                for (let ingredients of punchStarter.ingredients) {
                     html += `<li>${ingredients}</li>`;
                 }
-                html+= `</div>`;
-                html += `<div>`+
-                         `<label>Recipe</label>`+
-                         `<p>${punchStarter.recipe}</p>`+
-                        `</div>`;
+                html += `</div>`;
+                html += `<div>` +
+                    `<label>Recipe</label>` +
+                    `<p>${punchStarter.recipe}</p>` +
+                    `</div>`;
                 break;
                 break;
             case 'Crafts':
-                html += `<div>`+
+                html += `<div>` +
                     `<label>Resources</label>`;
-                for(let resources of punchStarter.resources){
+                for (let resources of punchStarter.resources) {
                     html += `<li>${resources}</li>`;
                 }
-                html+= `</div>`;
+                html += `</div>`;
                 break;
         }
         $('.punch-starter-lists').html(html);
@@ -105,10 +107,12 @@ class PunchStarterModel {
                     .append(($('<button>').text('Donate').on('click', function () {
                         let moneyToAdd = Number($('#donate-input').val());
                         let accumulatedMoney = punchStarter.accumulatedMoney();
-                        punchStarter.accumulatedMoney(accumulatedMoney+moneyToAdd);
+                        punchStarter.accumulatedMoney(accumulatedMoney + moneyToAdd);
 
                     }))));
-
+        $('.progress-bar-inner').css('width', (progress < 10 ? (progress * 0.7) + "vw" : "70vw"));
+        $('.punch-starter-progress').html(html);
+        
     }
 }
 
