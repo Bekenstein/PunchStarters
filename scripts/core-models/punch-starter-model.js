40153b4a10660f33584541991f06acdb92ff45c5
fiher <fihercho@gmail.com>
2016-11-11 21:00:02 +0200

    SwaggitySwootyCominForThatBonus

--- scripts/core-models/punch-starter-model.js
+++ scripts/core-models/punch-starter-model.js
@@ -4,35 +4,57 @@ class PunchStarterModel {
 
     }
     render(punchStarter){
-     let html = `<div class = "punch-starter-wrapper">                                   `+
-             `<div class = "punch-starter-caption">                                      `+
-             `<label>${punchStarter.name}</label>                                        `+
-             `<label>${punchStarter.constructor.name.replace('PunchStarter','')}</label> `+
-             `</div>                                                                     `+
-             `<div class = "punch-starter-resume">                                       `+
-             `<label>Manufacturer</label>                                                `+
-             `<p>${punchStarter.manufacturer}</p>                                        `+
-             `<label>Description</label>                                                 `+
-             `<p>${punchStarter.description}</p>                                         `+
-             `</div>                                                                     `+
-             `<div class = "punch-starter-lists">                                        `+
-             `</div>                                                                     `+
-             `<div class = "punch-starter-progress">                                     `+
-             `</div>                                                                     `+
-             `</div>                                                                     `;
+     let html = `<div class = "punch-starter-wrapper">                                          `+
+                  `<div class = "punch-starter-caption">                                        `+
+                    `<label>${punchStarter.name}</label>                                        `+
+                    `<label>${punchStarter.constructor.name.replace('PunchStarter','')}</label> `+
+                  `</div>                                                                       `+
+                  `<div class = "punch-starter-resume">                                         `+
+                    `<label>Manufacturer</label>                                                `+
+                    `<p>${punchStarter.manufacturer}</p>                                        `+
+                    `<label>Description</label>                                                 `+
+                    `<p>${punchStarter.description}</p>                                         `+
+                  `</div>                                                                       `+
+                  `<div class = "punch-starter-lists">                                          `+
+                  `</div>                                                                       `+
+                  `<div class = "punch-starter-progress">                                       `+
+                  `</div>                                                                       `+
+                `</div>                                                                         `;
         $('.wrapper main').html(html);
     }
     attachEvents(punchStarter){
 
     }
     renderLists(punchStarter){
-    let html = `<div>                                        `+
-            `<label>Genres</label>                           `+
-            `<ul>                                            `+
-            `<li>                                            `+
-            `</ul>                                           `+
-            '</div>                                          ';
+    let html = `<div>                                                                           `+
+                 `<label>Genres</label>                                                         `+
+                 `<ul>                                                                          `;
+        for(let obj of punchStarter){
+            html+= `<li>${obj.genres}</li>\n`;
+        }
+        html+= `</ul>                                                                           `+
+                `</div>                                                                         `;
+        switch(punchStarter.constructor.name.replace('PunchStarter','')){
+            case 'Movie':
+                html += `<div>`+
+                          `<label>Actors</label>`+
+                          `<ul>`;
+                for(let obj of punchStarter){
+                    html+= `<li>${obj.actor}</li>\n`;
+                }
+                html+= `</ul>`+
+                        `</div>`;
+                html+=`<div>`+
+                        `<label>Director</label>`+
+                        `<p>${punchStarter.director}</p>`+
+                      `</div>`;
+                break;
+            case 'Game':
+                
+        }
+
     }
+
     renderProgress(punchStarter){
 
     }
