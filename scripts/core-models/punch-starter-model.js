4681d0b56cb9e7817a4225b9403eef60a775fc35
fiher <fihercho@gmail.com>
2016-11-11 19:56:39 +0200

    SwaggitySwootyCominForThatBonus

--- scripts/core-models/punch-starter-model.js
+++ scripts/core-models/punch-starter-model.js
@@ -1,5 +1,36 @@
 class PunchStarterModel {
 	//TODO: Implement Punch Starter Model
+    constructor(){
+
+    }
+    render(punchStarter){
+     let html = `<div class = "punch-starter-wrapper">       `+
+             `<div class = "punch-starter-caption">          `+
+             `<label>${punchStarter.name}</label>            `+
+             `<label>${punchStarter.type}</label>            `+
+             `<div class = "punch-starter-resume">           `+
+             `<label>Manufacturer</label>                    `+
+             `<p>${punchStarter.manufacturer}</p>            `+
+             `<label>Description</label>                     `+
+             `<p>${punchStarter.description}</p>             `+
+             `<div class = "punch-starter-lists">            `+
+             `<div class = "punch-starter-progress">         `+
+             `</div>                                         `+
+             `</div>                                         `+
+             `</div>                                         `+
+             `</div>                                         `+
+             `</div>                                         `;
+        $('.wrapper main').html(html);
+    }
+    attachEvents(punchStarter){
+
+    }
+    renderLists(punchStarter){
+
+    }
+    renderProgress(punchStarter){
+
+    }
 }
 
 module.exports = PunchStarterModel;
