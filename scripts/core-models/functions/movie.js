d9f4f2acd4ca5c8576f0b89ba0267c9287395aa0
Georgi Georgiev <gogo4ds@gmail.com>
2016-11-11 16:28:51 +0200

    movie

--- scripts/core-models/functions/movie.js
+++ scripts/core-models/functions/movie.js
@@ -2,8 +2,14 @@ function render(){
     let html = '<label>Director:</label>' +
         '<div class="input-director"><input type="text" placeholder="Director...">' +
      '<label>Actors:</label>' +
+            '<div class="list-holder"><select class="input-actors"></select></div>' +
+            '<div><input class="new-actor" placeholder="Add actor..."></div>' +
+            '<div class="button-holder">' +
+                '<button class="add-actor-button">Add</button>' +
+                '<button class="remove-actor-button">Add</button>' +
+            '</div>'+
      '</div>';
-    //$('.wrapper main').append(html);
+    $('.individual-parameters').append(html)
 }
 
 function attachEvents(){
