98f2496bb6b62859a07038c7939e2508420133bf
fiher <fihercho@gmail.com>
2016-11-11 18:37:43 +0200

    SwaggitySwootyCominForThatBonus

--- scripts/core-models/functions/crafts.js
+++ scripts/core-models/functions/crafts.js
@@ -1,8 +1,26 @@
 function render(){
-
+    let html = '<label>Resources needed:</label>' +
+        '<div class="list-holder"><select class="input-resources"></select></div>' +
+        '<div><input class="new-resource" placeholder="Add resource..."></div>' +
+        '<div class="button-holder">' +
+        '<button class="add-resource-button">Add</button>' +
+        '<button class="remove-resource-button">Remove</button>' +
+        '</div>';
+    $('.individual-parameters').html(html)
 }
 
 function attachEvents(){
+    $('.add-resource-button').on('click', function () {
+        let input = $('.new-resource');
+        let resource = input.val();
+        $('.input-resources').append($('<option>').val(resource).text(resource));
+        input.val('');
+    });
 
+    $('.remove-resource-button').on('click', function () {
+        let select = $('.input-resources');
+        let resource = select.find(':selected');
+        select.find(resource).remove();
+    });
 }
 module.exports = {render, attachEvents};
