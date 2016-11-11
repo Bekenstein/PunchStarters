5e6a58e12f04b48086efd0d9a24bb196e9d2dc5a
Georgi Georgiev <gogo4ds@gmail.com>
2016-11-11 17:33:24 +0200

    food

--- scripts/core-models/functions/food.js
+++ scripts/core-models/functions/food.js
@@ -1,8 +1,27 @@
 function render(){
-
+    let html = '<label>Actors:</label>' +
+        '<div class="list-holder"><select class="input-ingredients"></select></div>' +
+        '<div><input class="new-ingredient" placeholder="Add ingredients..."></div>' +
+        '<div class="button-holder">' +
+        '<button class="add-ingredient-button">Add</button>' +
+        '<button class="remove-ingredient-button">Remove</button>' +
+        '</div>'+
+        '<label>Recipe:</label>' +
+        '<div class="input-recipe"><input type="text" placeholder="Recipe..."></div>';
 }
 
 function attachEvents(){
+    $('.add-ingredient-button').on('click', function () {
+        let input = $('.new-ingredient');
+        let ingredient = input.val();
+        $('.input-ingredients').append($('<option>').val(ingredient).text(ingredient));
+        input.val('');
+    });
 
+    $('.remove-ingredient-button').on('click', function () {
+        let select = $('.input-ingredients');
+        let ingredient = select.find(':selected');
+        select.find(ingredient).remove();
+    });
 }
 module.exports = {render, attachEvents};
