389402bb1f2322f79f16cc2c855a6b8bf44e66ac
Georgi Georgiev <gogo4ds@gmail.com>
2016-11-11 20:58:45 +0200

    create punchStarterModel with ID

--- scripts/core-models/create-model.js
+++ scripts/core-models/create-model.js
@@ -3,7 +3,12 @@ let game = require('./functions/game.js');
 let innovative = require('./functions/innovative.js');
 let food = require('./functions/food.js');
 let crafts = require('./functions/crafts.js');
-let submitButton = require('./functions/submit.js');
+let MoviePunchStarter = require('../punch-starters/movie-punch-starter.js');
+let GamePunchStarter = require('../punch-starters/game-punch-starter.js');
+let FoodPunchStarter = require('../punch-starters/food-punch-starter.js');
+let CraftsPunchStarter = require('../punch-starters/crafts-punch-starter.js');
+let InnovativePunchStarter = require('../punch-starters/innovative-punch-starter.js');
+let id = 6;
 
 class CreateModel {
     constructor() {
@@ -108,10 +113,7 @@ class CreateModel {
             select.find(gender).remove();
         });
 
-
-
-
-        $('.submit-button-holder').on('click', submitButton.submit);
+        $('.submit-button-holder').on('click', submit);
     }
 
     renderCreateMovieModel() {
@@ -149,6 +151,51 @@ class CreateModel {
     attachEventsCreateCraftsModel() {
         crafts.attachEvents();
     }
+}
+
+function submit() {
+    let selectedModel = $('.punch-starter-category select').find(':selected').val();
+    let punchStarter = {};
+    let name = $('.input-name').val();
+    let manufacturer = $('.input-manufacturer').val();
+    let description = $('.input-description').val();
+    let genres = addItemsFromList('.input-genres');
+    let options = $('.input-genres').find('option');
+    let targetPrice = Number($('.input-target-price'));
+    switch (selectedModel){
+        case 'Movie':
+            let director = $('.input-director input').val();
+            let actors = addItemsFromList('.input-actors');
+            punchStarter = new MoviePunchStarter(id, name, manufacturer, description, genres, targetPrice, director, actors);
+            break;
+        case 'Game':
+            let technologiesUsed = addItemsFromList('.input-technologies');
+            punchStarter = new GamePunchStarter(id, name, manufacturer, description, genres, targetPrice, technologiesUsed);
+            break;
+        case 'Food':
+            let ingredients = addItemsFromList('.input-ingredients');
+            let recipe = $('.input-recipe input').val();
+            punchStarter = new FoodPunchStarter(id, name, manufacturer, description, genres, targetPrice, ingredients, recipe);
+            break;
+        case 'Crafts':
+            let resources = addItemsFromList('.input-resources');
+            punchStarter = new CraftsPunchStarter(id, name, manufacturer, description, genres, targetPrice, resources);
+            break;
+        case 'Innovative':
+            punchStarter = new InnovativePunchStarter();
+            break;
+    }
+    id++;
+    $('.wrapper main').trigger('createPunchStarter', [punchStarter]);
+}
+
+function addItemsFromList(listSelector) {
+    let arr = [];
+    let options = $(listSelector).find('option');
+    for (let opt of options){
+        arr.push(opt.value);
+    }
+    return arr;
 }
 
 module.exports = CreateModel;
