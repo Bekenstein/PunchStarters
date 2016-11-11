a81992fb707ca4047ac327b4a11f1de22c5d18f4
dantcho <dantcho@abv.bg>
2016-11-11 17:32:56 +0200

    divStructure

--- scripts/core-models/create-model.js
+++ scripts/core-models/create-model.js
@@ -19,15 +19,28 @@ class CreateModel {
                 .append(selectList));
         let form = $('<form>')
             .append($('<div>').addClass('main-parameters')
-                .append($('<label>')
-                    .append($('<div>').addClass('input-holder')))
-                  .append($('<label>')
-                    .append($('<div>').addClass('input-holder')))
-                .append($('<label>')
-                    .append($('<div>').addClass('input-holder')))
-            )
-            .append($('<div>').addClass('secondary-parameters'))
+                .append($('<label>').text('Name:')
+                    .append($('<div>').addClass('input-holder')
+                        .append($('<input>').addClass('input-name').attr('type', 'text').attr('value', 'Name...').attr('maxlength', '20'))))
+                .append($('<label>').text('Manufacturer:')
+                    .append($('<div>').addClass('input-holder')
+                        .append($('<input>').addClass('input-manufacturer').attr('type', 'text').attr('value', 'Manufacturer...').attr('maxlength', '20'))))
+                .append($('<label>').text('Description:')
+                    .append($('<div>').addClass('input-holder')
+                        .append($('<textarea>').addClass('input-description').attr('type', 'text').attr('value', 'Description...').attr('rows', '2')))))
+
+            .append($('<div>').addClass('secondary-parameters')
+                .append($('<label>').text('Genres:')
+                    .append($('<div>').addClass('list-holder')
+                        .append($('<select>').addClass('input-genres'))
+                        .append($('<div>')
+                            .append($('<input>').addClass('input-genre').attr('type', 'text').attr('value', 'Add genre...')))
+                        .append($('<div>').addClass('button-holder')
+                            .append($('<button>').addClass('add-genre-button').text('Add'))
+                            .append($('<button>').addClass('remove-genre-button').text('Remove'))))))
+
             .append($('<div>').addClass('individual-parameters'));
+
 
         let formHolder = $('<div>').addClass('create-form-holder')
             .append(form)
@@ -45,11 +58,39 @@ class CreateModel {
 
     attachEvents() {
         $('.punch-starter-category select').on('change', function (ev, data, selector) {
-            let a = data;
-            let e = ev;
-            let b = 6;
+            let val = this.value;
+            switch (val) {
+                case 'Movie':
+                {
+                    this.renderCreateMovieModel();
+                    this.attachEventsCreateMovieModel();
+                    break;
+                }
+                case 'Game':
+                {
+                    this.renderCreateGameModel();
+                    this.attachEventsCreateGameModel();
+                    break;
+                }
+                case 'Innovative':
+                {
+                    this.renderCreateInnovativeModel();
+                    break;
+                }
+                case 'Food':
+                {
+                    this.renderCreateFoodModel();
+                    this.attachEventsCreateFoodModel();
+                    break;
+                }
+                case 'Crafts':
+                {
+                    this.renderCreateCraftsModel();
+                    this.attachEventsCreateCraftsModel();
+                    break;
+                }
+            }
         });
-        //TODO:
     }
 
     renderCreateMovieModel() {
